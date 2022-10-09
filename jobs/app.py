60d206cafcd29b453976dff6eec245790bad6328
Mahat Mohamed <Mahatmohamed@mail.weber.edu>
2022-10-08 23:19:51 -0500

    Completed module

--- jobs/app.py
+++ jobs/app.py
@@ -17,7 +17,7 @@ def open_connection():
 def execute_sql(sql, values=(), commit= False,single= False):
     connection = open_connection()
     cursor = connection.execute(sql,values)
-    if commit == true:
+    if commit == True:
         results = connection.commit()
     else:
         results = cursor.fetchone() if single else cursor.fetchall()
